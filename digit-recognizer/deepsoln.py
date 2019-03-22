8b1bad7c5f40262e09a90fc0ca61f926937813ab
greengangsta <36106756+greengangsta@users.noreply.github.com>
2019-03-22 15:55:04 +0530

    optimization
    accuracy =97.328

---
+++ digit-recognizer/deepsoln.py
@@ -0,0 +1,81 @@
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#Loading the dataset
+td = pd.read_csv('train.csv')
+ts = pd.read_csv('test.csv')
+  
+#Selecting out the training and test data's dependent and independent variables
+X=td.iloc[:,1:].values
+y=td.iloc[:,0].values
+x=ts.iloc[:,:].values
+
+
+#Splitting the traing data into training and cross validation
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+
+#Scaling the input features of training and test data
+
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+x=sc_X.transform(x)
+
+y_train=y_train.reshape((33600,1))
+y_test = y_test.reshape((8400,1))
+
+from sklearn.preprocessing import OneHotEncoder
+onehotencoder = OneHotEncoder(categorical_features =[0])
+y_train= onehotencoder.fit_transform(y_train).toarray()
+y_test = onehotencoder.fit_transform(y_test).toarray()
+
+
+from keras.models import Sequential
+from keras.layers import Dense
+
+classifier = Sequential()
+
+classifier.add(Dense(units = 800,activation='relu',input_dim=784,init='uniform'))
+
+classifier.add(Dense(units = 800,activation='relu',init='uniform'))
+
+classifier.add(Dense(units = 400,activation='relu',init='uniform'))
+
+classifier.add(Dense(units = 200,activation='relu',init='uniform'))
+
+classifier.add(Dense(units = 64,activation='relu',init='uniform'))
+
+classifier.add(Dense(units = 64,activation='relu',init='uniform'))
+
+classifier.add(Dense(units = 32,activation='relu',init='uniform'))
+
+classifier.add(Dense(units = 10,activation='softmax',init='uniform'))
+
+classifier.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
+
+classifier.fit(X_train,y_train,batch_size=64,epochs=50)
+
+y_pred_ann = classifier.predict(X_test)
+
+y_pred_ann = np.argmax(y_pred_ann,axis=-1)
+y_test = np.argmax(y_test,axis=-1)
+
+y_ann = classifier.predict(x)
+y_ann = np.argmax(y_ann,axis=-1)
+
+cmann = confusion_matrix(y_test,y_pred_ann)
+
+y_num=np.column_stack((np.arange(1,28001,dtype=np.int64) ,y_ann))
+y_ann_sol = pd.DataFrame(y_num,columns=['ImageId','Label']).to_csv('y_ann_sol.csv')
+
+
+
+
+
+
+
